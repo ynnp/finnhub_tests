b6dbafea08fc6cc8a4c3467342023203291c712c
Yana Petrachenko <Yana.Petrachenko@emc.com>
2022-07-02 15:21:38 +0300

    Added session to RestApiClient

--- rest_api_client.py
+++ rest_api_client.py
@@ -9,12 +9,14 @@ class RestApiClient(object):
     HOST = "finnhub.io"
     
     def __init__(self, token: str) -> None:
-        self.__token = token
-        self.__header = self.__get_header()
         self.api_url = self.get_api_url()
+        self.__token = token
+        self.__auth_header = self.__get_auth_header()
+        self.__session = requests.session()
+        self.__session.headers.update(self.__auth_header)
         
     
-    def __get_header(self) -> dict:
+    def __get_auth_header(self) -> dict:
         header = {'X-Finnhub-Token': self.__token}
         
         return header
@@ -28,7 +30,7 @@ class RestApiClient(object):
         
     def get_symbols_for_forex_exchange(self, forex_exchange: str) -> list:
         symbol_url = "{api_url}/symbol?exchange={forex_exchange}".format(api_url=self.api_url, forex_exchange=forex_exchange)
-        symbols_data = requests.get(symbol_url, headers=self.__header)
+        symbols_data = self.__session.get(symbol_url)
         symbols = []
         for obj in symbols_data.json():
             symbols.append(obj["symbol"])
@@ -42,7 +44,7 @@ class RestApiClient(object):
         logger.info("Accessing {endpoint} endpoint for {count} times.".format(endpoint=endpoint.upper(), count=count))
         for _ in range(count):
             start_time = time.perf_counter()
-            requests.get(url, params=params, headers=self.__header)
+            self.__session.get(url, params=params)
             elapsed_time = time.perf_counter() - start_time
             response_time.append(elapsed_time)
         
